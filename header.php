c31856289eddd1bec3cd2814c8563b85ebc4526f
Alexa <65460924+lexausart@users.noreply.github.com>
2023-12-25 09:32:51 -0500

    testing making header.php

---
+++ header.php
@@ -0,0 +1,87 @@
+<!-- Head-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://sari.lexa.us" />
+    <meta property="og:title" content="Sari-Sari Store" />
+    <meta property="og:description" content="Your favorite convenience store!" />
+    <meta property="og:image" content="https://imgur.com/4sTLTXV.gif" />
+    <meta charset="UTF-8">
+    <meta name="twitter:card" content="summary">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Sari-Sari Store</title>
+</head>
+
+<body>
+
+
+
+    <!-- Account-->
+    <div class="triangle"><i class="fa-solid fa-triangle"></i></div>
+    <div class="hdn-sign">
+        <div class="hdn-table">
+            <div>
+                <h3>Your Account</h3>
+                <ul>
+                    <li>Account</li>
+                    <li>Orders</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="black"></div>
+    <header>
+        <div class="first">
+
+            <!-- Logo-->
+            <div class="flex logo">
+                <a href="https://sari.lexa.us"><img src="images/logo3.png" alt=""></a>
+
+            </div>
+
+
+            <!-- Account part 2-->
+            <div class="flex right">
+                <div class="sign">
+                    <span>Hello, Sign in</span>
+                    <div class="flex ac">
+                        <span>Account</span>
+                        <i class="fas fa-caret-down"></i>
+                    </div>
+                </div>
+
+                <!-- Cart-->
+                <div class="flex cart">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span class="ca">Cart</span>
+                </div>
+            </div>
+        </div>
+        <div class="second">
+            <div class="second-1">
+
+                <!-- Main menu-->
+                <div>
+                    <i class="fas fa-map-marker"></i>
+                    <span>Balmung</span>
+                </div>
+            </div>
+
+            <!-- Menu address-->
+            <div class="second-2">
+                <p>
+                    <span>Mist Ward 7 Apt 55</span>
+                </p>
+            </div>
+
+        </div>
+    </header>
